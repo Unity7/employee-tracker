582536e27f49c37b3de95d4103b5a4dabd3366c0
unknown <baybui@ucla.edu>
2021-05-08 17:06:46 -0800

    updated security

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: "root",
     // Your MySQL password
-    password: "Infinity369#1",
+    password: "data",
     database: "employees",
   },
   console.log("Connected to the employees database.")
